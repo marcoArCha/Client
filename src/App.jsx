15815b9cbc2422b363ef4e8d0b989d002d6249d5
Marco Arias <marcoarccha@hotmail.com>
2024-05-18 06:07:34 -0600

    Basic router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  //Each route is going to be an object
+  {
+    //This is just an example
+    path: '/',
+    element: <h1>Home page</h1>
+  },
+    {
+    //This is just an example
+    path: '/about',
+    element: <h1>About page</h1>
+  }
+]);
 
 const App = () => {
   return (
-    <h1>JobQuest App</h1>
-  )
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App;
-
-
-import React from 'react'
-
-
