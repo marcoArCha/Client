07413721194068388f96331094a0b68bb6b58ebb
Marco Arias <marcoarccha@hotmail.com>
2024-05-18 09:57:00 -0600

    All pages were imported, some routes defined.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import HomeLayout from './pages/HomeLayout';
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
+} from "./pages";
 
 const router = createBrowserRouter([
   //Each route is going to be an object
   {
-    //This is just an example
-    path: '/',
-    element: <HomeLayout/>
+    path: "/",
+    element: <HomeLayout />,
   },
-    {
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
     //This is just an example
-    path: '/about',
-    element: <h1>About page</h1>
-  }
+    path: "/dashboard",
+    element: <DashboardLayout />,
+  },
 ]);
 
 const App = () => {
-  return (
-    <RouterProvider router={router}/>
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
 export default App;
