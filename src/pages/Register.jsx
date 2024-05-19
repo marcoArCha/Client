32768f09b274709598ee7f0e0da3f98a152af6b1
Marco Arias <marcoarccha@hotmail.com>
2024-05-19 15:33:32 -0600

    Register page implemented

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="Marco" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="Last Name"
+          defaultValue="Arias"
+        />
+        <FormRow type="text" name="location" defaultValue="Costa Rica" />
+        <FormRow type="email" name="email" defaultValue="marco@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="12345" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
