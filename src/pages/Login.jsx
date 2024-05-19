a69cc5a5b8e5110e40a9c5daade508ff281c4a6b
Marco Arias <marcoarccha@hotmail.com>
2024-05-19 06:12:07 -0600

    Basic Link component implemented

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <h1>Login page</h1>
+      <Link to="/Register">Register Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Login
-
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
     <div>
       <h1>Register page</h1>
+      <Link to="/login">Login Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
