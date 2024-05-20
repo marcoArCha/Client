eef0d0ae2c79645c6750a60ad09f7450bc5d31ac
Marco Arias <marcoarcha@hotmail.com>
2024-05-20 06:59:59 -0600

    Dashboard layout implemented.

---
+++ src/components/SmallSidebar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+const SmallSidebar = () => {
+  return <Wrapper>Small sidebar</Wrapper>;
+};
+
+export default SmallSidebar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Logo } from "../components/Logo";
 export { default as FormRow } from "../components/FormRow";
+export { default as BigSidebar } from "../components/BigSidebar";
+export { default as SmallSidebar } from "../components/SmallSidebar";
+export { default as Navbar } from "../components/Navbar";
