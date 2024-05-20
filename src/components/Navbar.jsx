5fc6843a878afe139f6be5908ab98edd0d19121e
Marco Arias <marcoarcha@hotmail.com>
2024-05-20 09:59:19 -0600

    Navbar structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import { useDashboardContext } from "../pages/DashboardLayout";
 
 const Navbar = () => {
+  const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
-      Navbar
-      <FaHome />
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">Dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
     </Wrapper>
   );
 };
