1363527528f308f639425dbd4d014e7ec6ca368e
Marco Arias <marcoarcha@hotmail.com>
2024-05-21 07:01:05 -0600

    Small sibebar funtionality completed

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -7,11 +7,16 @@ import links from "../utils/links";
 import { NavLink } from "react-router-dom";
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
         <div className="content">
-          <button type="button" className="close-btn">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
             <FaTimes />
           </button>
 
@@ -22,7 +27,13 @@ const SmallSidebar = () => {
             {links.map((link) => {
               const { text, path, icon } = link;
               return (
-                <NavLink to={path} key={text} className="nav-link">
+                <NavLink
+                  to={path}
+                  key={text}
+                  className="nav-link"
+                  onClick={toggleSidebar}
+                  end
+                >
                   <span className="icon">{icon}</span>
                   {text}
                 </NavLink>
